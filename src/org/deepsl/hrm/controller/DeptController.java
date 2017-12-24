8deab828357cbae055b93f0ca8e7e3e55ed41b2f
wwj <wwj@gmail.com>
2017-12-24 21:33:05 +0800

    add mybatis

--- src/org/deepsl/hrm/controller/DeptController.java
+++ src/org/deepsl/hrm/controller/DeptController.java
@@ -3,6 +3,7 @@ package org.deepsl.hrm.controller;
 import java.util.List;
 
 import org.deepsl.hrm.domain.Dept;
+
 import org.deepsl.hrm.service.HrmService;
 import org.deepsl.hrm.util.tag.PageModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +14,43 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 /**   
- * @Description: 澶勭悊閮ㄩ棬璇锋眰鎺у埗鍣� * @author   
+ * @Description: 处理部门请求控制器
+ * @author   
  * @version V1.0   
  */
-
+@RequestMapping("dept")
 @Controller
 public class DeptController {
+	
+/*	@Autowired
+	DeptService service;
+	
+	@RequestMapping("findAllDept") 
+	public String findAllDept(Model model){
+		
+		List<Dept> selectAllDept = service.selectAllDept();
+		if (selectAllDept!=null) {
+			model.addAttribute("depts", selectAllDept);
+			
+			return "/jsp/dept/dept.jsp";
+		}else {
+			return "/jsp/dept/deptError.jsp";
+		}
+						
+	}
+	
+	@RequestMapping("deleteDeptById")
+	public String deleteDeptById(int id){
+		try{
+		  service.deleteById(id);
+						
+		}catch(Exception e){
+			return "/jsp/dept/deptError.jsp";
+		}
+		return "/jsp/dept/dept.jsp";
 
-	
-	 //add a test note for push
-	 //add a test note for push2
-	
- 
+	}
+ */
 }
